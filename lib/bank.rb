4ff1564ab7f5a682facb599bafa4babdabd55db2
Matthew <matt_burgoyne@live.co.uk>
2018-05-21 13:14:40 +0100

    user can add amount to their balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,9 +1,18 @@
 class Bank
-  attr_reader :balance
+  attr_accessor :balance
   def initialize
     @balance = 0
   end
   def deposit
     puts 'Please enter the amount you would like to deposit'
+    make_deposit(get_user_amount)
+
+  end
+  private
+  def get_user_amount
+    gets.chomp
+  end
+  def make_deposit(amount)
+    self.balance += amount.to_i
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,6 +3,7 @@ require 'simplecov'
 
 describe Bank do
   subject(:bank) { described_class.new }
+
   it 'has a default balance of 0' do
     expect(bank.balance).to eq 0
   end
@@ -10,5 +11,12 @@ describe Bank do
     # act
     # assert
     expect{ bank.deposit }.to output("Please enter the amount you would like to deposit\n").to_stdout
+  end
+  it 'adds the users amount to the bank balance' do
+    ## act
+    bank.send :make_deposit, "10"
+
+    expect(bank.balance).to eq 10
+
   end
 end
