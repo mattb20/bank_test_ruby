2f261de9d95ea11e5047ccad2cd1e6ebff679f42
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 10:23:25 +0100

    transaction deposit test expectation failing

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,6 +5,6 @@ describe Transaction do
     let(:transaction) { Transaction.new };
     let(:bank) { double :bike, :balance => 10 };
   it 'will deposit a given amount into a bank balance' do
-    transaction.complete(bank);
+    expect(transaction.complete(bank)).to eq(15);
     end
 end
