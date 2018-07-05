5d32e292906a9c7091f86b283da8ec9a0666e34d
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 10:29:29 +0100

    corrected bike to bank

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,8 +2,8 @@ require 'transaction'
 require 'date'
 require 'bank'
 describe Transaction do
-    let(:transaction) { Transaction.new };
-    let(:bank) { double :bike, :balance => 10 };
+    let(:transaction) { Transaction.new('deposit', 5) };
+    let(:bank) { double :bank, :balance => 10 };
   it 'will deposit a given amount into a bank balance' do
     expect(transaction.complete(bank, 5)).to eq(15);
     end
