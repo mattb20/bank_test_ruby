e2b87aa39a3f6594c442b17585b05397b3ed8bd9
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 10:46:40 +0100

    failing test for transaction withdraw decrease bank balance by given amount

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,7 @@
 class Transaction
-  attr_reader :transaction_type, :amount
-  def initialize(transaction_type, amount)
+  attr_reader :transaction_type
+  def initialize(bank, transaction_type, amount)
     @transaction_type = transaction_type
-    @amount = amount
   end
   def complete(bank, amount)
     case self.transaction_type
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,12 +1,17 @@
 require 'transaction'
 require 'date'
-require 'bank'
 describe Transaction do
-    let(:transaction) { Transaction.new('deposit', 5) };
     let(:bank) { double 'bank' };
   it 'will deposit a given amount into a bank balance' do
+    transaction = Transaction.new(bank, 'deposit', 5);
     allow(bank). to receive(:balance).and_return(10);
     allow(bank). to receive(:balance=).and_return(15);
     expect(transaction.complete(bank, 5)).to eq(15);
+    end
+    it 'will withdraw a given amount from a bank balance' do
+      transaction = Transaction.new(bank, 'withdraw', 4);
+      allow(bank). to receive(:balance).and_return(5);
+      allow(bank). to receive(:balance=).and_return(1);
+      expect(transaction.complete(bank, 4)).to eq(1);
     end
 end
