d9189d4e172fbf34698ab9540e5f843c3f0ea353
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 14:34:30 +0100

    stashing, printer spec has weird typerror of conversion of array to integer

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,5 +2,8 @@ class Printer
   def initialize
   end
   def output(bank)
+    bank.transaction_history.each do |transaction|
+      puts transaction.join( ' || ')
+  end
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,13 +1,10 @@
 require 'printer'
 require 'date'
-
 describe Printer do
-  let(:bank) { double('bank') }
   let(:printer) { described_class.new }
+  let(:bank) { double 'bank', :transaction_history [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']] }
   it 'will print something as expected' do
-    allow(bank).to receive(:transaction_history) { [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']] };
     expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n22/05/2018 || 10.00 ||   || 10.00\n");
     printer.output(bank);
   end
-
 end
