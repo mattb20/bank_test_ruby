f745a3231a45046f55db6595b8c38681e52d7ca5
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 10:37:26 +0100

    transaction deposit test increase  by amount now passing

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,8 +3,10 @@ require 'date'
 require 'bank'
 describe Transaction do
     let(:transaction) { Transaction.new('deposit', 5) };
-    let(:bank) { double :bank, :balance => 10 };
+    let(:bank) { double 'bank' };
   it 'will deposit a given amount into a bank balance' do
+    allow(bank). to receive(:balance).and_return(10);
+    allow(bank). to receive(:balance=).and_return(15);
     expect(transaction.complete(bank, 5)).to eq(15);
     end
 end
