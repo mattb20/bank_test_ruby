e761ffa3c9779cdffbf0b79b71edc33bd8cd2652
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 17:21:22 +0100

    printer test for output method without mocking the bank

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,10 +1,12 @@
 require 'printer'
+require 'bank'
 require 'date'
 describe Printer do
   let(:printer) { described_class.new }
-  let(:bank) { double 'bank', :transaction_history [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']] }
   it 'will print something as expected' do
-    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n22/05/2018 || 10.00 ||   || 10.00\n");
+    bank = Bank.new
+    bank.transaction_history = [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']]
+    expect{ printer.output(bank) }.to output("date || credit || debit || balance\n22/05/2018 || 10.00 ||   || 10.00\n").to_stdout
     printer.output(bank);
   end
 end
