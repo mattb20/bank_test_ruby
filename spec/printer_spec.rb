524563e16369080c442de6c272d1e579695d18b0
mattb20 <matt_burgoyne@live.co.uk>
2018-07-05 17:24:15 +0100

    printer spec output test now passes through use of bank double correctly stubbed with arrow

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -3,9 +3,8 @@ require 'bank'
 require 'date'
 describe Printer do
   let(:printer) { described_class.new }
+  let(:bank) { double('bank', :transaction_history => [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']])}
   it 'will print something as expected' do
-    bank = Bank.new
-    bank.transaction_history = [["date || credit || debit || balance"],["22/05/2018", '10.00', " ", '10.00']]
     expect{ printer.output(bank) }.to output("date || credit || debit || balance\n22/05/2018 || 10.00 ||   || 10.00\n").to_stdout
     printer.output(bank);
   end
